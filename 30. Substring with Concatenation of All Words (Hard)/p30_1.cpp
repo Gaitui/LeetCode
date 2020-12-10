791125840e030f2d028513b2058295421dfc47ba
Gaitui <52013678+Gaitui@users.noreply.github.com>
2020-12-11 04:26:34 +0900

    Create p30_1.cpp

---
+++ 30. Substring with Concatenation of All Words (Hard)/p30_1.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        int ws=words.size();
+        int ss=words[0].size();
+        bool *flag=new bool[ws];
+        bool find;
+        int refind;
+        vector<int> order;
+        vector<int> ans;
+        string sub;
+        //cout<<ws<<" "<<ss<<endl;
+        for(int x=0;x<s.size()-ss*ws+1;x++)
+        {
+            for(int f=0;f<ws;f++)
+            {
+                flag[f]=true;
+            }
+            order.clear();
+            for(int i=x;i<s.size()-ss+1;i+=ss)
+            {
+                find=false;
+                sub=s.substr(i,ss);
+                for(int f=0;f<ws;f++)
+                {
+                    if(flag[f])
+                    {
+                        if(words[f].compare(sub)==0)
+                        {
+                            flag[f]=false;
+                            order.push_back(f);
+                            find=true;
+                            break;
+                        }
+                    }
+                }
+                if(find)
+                {
+                    //cout<<"find"<<endl;
+                    if(order.size()==ws)
+                    {
+                        //cout<<i<<endl;
+                        ans.push_back(i-(ws-1)*ss);
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
