5b0a2e7ea41fd6145ab4592d633b7533e1671d45
Gaitui <52013678+Gaitui@users.noreply.github.com>
2020-12-17 05:46:24 +0900

    Create p31_1.cpp

---
+++ 31. Next Permutation (Medium)/p31_1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        for(int i=nums.size()-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                for(int j=nums.size()-1;j>i;j--)
+                {
+                    if(nums[i]<nums[j])
+                    {
+                        swap(nums[i],nums[j]);
+                        break;
+                    }
+                }
+                reverse(nums.begin()+(i+1),nums.end());
+                return;
+            }
+        }
+        reverse(nums.begin(),nums.end());
+        return;
+    }
+};
