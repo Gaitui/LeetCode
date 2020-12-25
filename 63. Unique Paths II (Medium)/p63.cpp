ba4cd384fe31356941cb6799d11b2f4253aa3ea9
Gaitui <52013678+Gaitui@users.noreply.github.com>
2020-12-26 08:43:02 +0900

    Create p63.cpp

---
+++ 63. Unique Paths II (Medium)/p63.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if(obstacleGrid[0][0]==1 || obstacleGrid[obstacleGrid.size()-1][obstacleGrid[0].size()-1]==1)
+            return 0;
+        int dp[obstacleGrid.size()][obstacleGrid[0].size()];
+        dp[0][0]=1;
+        for(int i=1;i<obstacleGrid[0].size();i++)
+        {
+            if(obstacleGrid[0][i]==1)
+                dp[0][i]=0;
+            else
+                dp[0][i]=dp[0][i-1];
+        }
+        for(int i=1;i<obstacleGrid.size();i++)
+        {
+            if(obstacleGrid[i][0]==1)
+                dp[i][0]=0;
+            else
+                dp[i][0]=dp[i-1][0];
+        }
+        for(int i=1;i<obstacleGrid.size();i++)
+        {
+            for(int j=1;j<obstacleGrid[0].size();j++)
+            {
+                if(obstacleGrid[i][j]==0)
+                {
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+                else
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return dp[obstacleGrid.size()-1][obstacleGrid[0].size()-1];
+    }
+};
